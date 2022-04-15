fe80f06118fe087964f69b457734cb0906c786d6
astha05 <asthash77@gmail.com>
2022-04-15 10:00:05 +0530

    updated code

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const router = require('./Router/index');
 const cors=require('cors');
 const app = express();
 
-const port = 4567;
-const hostname = 'localhost';
+ const port =process.env.PORT || 2001;
+const hostname = '0.0.0.0';
 
 const localDB = 'mongodb://127.0.0.1:27017/zomato_27';
 const serverDB = 'mongodb+srv://zomato_27:astha11@cluster0.me61q.mongodb.net/Zomato_27?retryWrites=true&w=majority';
